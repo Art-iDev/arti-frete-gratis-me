a38f7f4dc135e5908c0cee04c24d20f63df533cf
Luis Eduardo Braschi <luis.eduardo.braschi@outlook.com>
2021-12-05 07:45:38 -0300

    Bumping version

--- arti-frete-gratis-me.php
+++ arti-frete-gratis-me.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://art-idesenvolvimento.com.br
  * Author: Luis Eduardo Braschi
  * Author URI: https://art-idesenvolvimento.com.br
- * Version: 0.1.1
+ * Version: 0.2.0
  * License: GPL2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: Text Domain
