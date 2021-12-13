989bca6ff644a13b361d7420144e616d6f33c6e8
Luis Eduardo Braschi <luis.eduardo.braschi@outlook.com>
2021-12-13 16:08:49 -0300

    Adding support to WCMP and refactoring the algorithm into one function

--- marketplace-functions.php
+++ marketplace-functions.php
@@ -4,37 +4,41 @@
  */
 
 add_action( 'arti_mpme_provider_dokan_loaded', function(){
-    add_filter( 'arti_fgme_cart_has_free_shipping', function( $cart_has_free_shipping, $free_shipping_methods, $methods ) {
-
-        if( !$cart_has_free_shipping ){
-
-            $free_shipping_methods = arti_fgme_get_accepted_methods();
-            $method_ids = array_flip( $methods );
-            $method_ids = preg_replace( '/\:[0-9]+/', '', $method_ids );
-
-            return isset( $method_ids['dokan_vendor_shipping'] ) && arti_fgme_cart_has_free_shipping( $free_shipping_methods, $method_ids );
-
-        }
-
-        return $cart_has_free_shipping;
-
-    }, 10, 3 );
+    add_filter( 'arti_fgme_get_vendor_shipping_key', function(){
+        return 'dokan_vendor_shipping';
+    } );
 } );
 
 add_action( 'arti_mpme_provider_wcfm_loaded', function(){
-    add_filter( 'arti_fgme_cart_has_free_shipping', function( $cart_has_free_shipping, $free_shipping_methods, $methods ) {
-
-        if( !$cart_has_free_shipping ){
-
-            $free_shipping_methods = arti_fgme_get_accepted_methods();
-            $method_ids = array_flip( $methods );
-            $method_ids = preg_replace( '/\:[0-9]+/', '', $method_ids );
-            write_log($method_ids);
-            return isset( $method_ids['wcfmmp_product_shipping_by_zone'] ) && arti_fgme_cart_has_free_shipping( $free_shipping_methods, $method_ids );
-
-        }
-
-        return $cart_has_free_shipping;
-
-    }, 10, 3 );
+    add_filter( 'arti_fgme_get_vendor_shipping_key', function(){
+        return 'wcfmmp_product_shipping_by_zone';
+    } );
 } );
+
+add_action( 'arti_mpme_provider_wc-marketplace_loaded', function(){
+    add_filter( 'arti_fgme_get_vendor_shipping_key', function(){
+        return 'wcmp_vendor_shipping';
+    } );
+} );
+
+add_filter( 'arti_fgme_cart_has_free_shipping', function( $cart_has_free_shipping, $free_shipping_methods, $methods ) {
+
+    if( !$cart_has_free_shipping ){
+
+        $free_shipping_methods = arti_fgme_get_accepted_methods();
+        $method_ids = array_flip( $methods );
+        $method_ids = preg_replace( '/\:[0-9]+/', '', $method_ids );
+
+        $free_shipping_key = arti_fgme_get_vendor_shipping_key();
+
+        return isset( $method_ids[$free_shipping_key] ) && arti_fgme_cart_has_free_shipping( $free_shipping_methods, $method_ids );
+
+    }
+
+    return $cart_has_free_shipping;
+
+}, 10, 3 );
+
+function arti_fgme_get_vendor_shipping_key(){
+    return apply_filters( 'arti_fgme_get_vendor_shipping_key', '' );
+}
